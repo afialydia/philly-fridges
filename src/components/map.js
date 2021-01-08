7fb4a0b315015f30520553d09d950c28517173db
Casey Caruso <caseycaruso@caseys-mbp.lan>
2021-01-08 17:26:19 -0500

    changes to map on home page and added germantown fridge information

--- src/components/map.js
+++ src/components/map.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import fridge from '../assets/phillyfridges11_9.PNG' 
+import fridge from '/Users/caseycaruso/Pictures/2020/philly_fridges/philly-fridges/src/assets/phillyfridges11_9_desktop.png' 
 import './components.css'
 
 
 const FridgeMap = () =>{
     return(
     <div className="map-container">
-      <img className="map" src={fridge}/>  <a download href={fridge}><button>Download Map</button></a>
+      <img className="map" src={fridge}/>  <a className="download-button" download href={fridge}><button>Download Map</button></a>
 
     </div>
     )
